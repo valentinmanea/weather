8decd6ef6fabda92eb537e2d41dfe92cfebe3e6d
Valentin Manea <valentin.manea9696@gmail.com>
2019-09-05 21:12:35 +0300

    s

--- Weather-App/src/main/java/com/weatherapp/WeatherApp/api/dto/DirectionDto.java
+++ Weather-App/src/main/java/com/weatherapp/WeatherApp/api/dto/DirectionDto.java
@@ -1,5 +1,8 @@
 package com.weatherapp.WeatherApp.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class DirectionDto {
+	@JsonProperty("Degrees")
 	public double degrees;
 }
--- Weather-App/src/main/java/com/weatherapp/WeatherApp/api/dto/SpeedDto.java
+++ Weather-App/src/main/java/com/weatherapp/WeatherApp/api/dto/SpeedDto.java
@@ -1,7 +1,11 @@
 package com.weatherapp.WeatherApp.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class SpeedDto {
+	@JsonProperty("Value")
 	public String value;
-	
+
+	@JsonProperty("Unit")
 	public String unit;
 }
